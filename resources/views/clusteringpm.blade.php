dd154d794e1d4f54e6e8077d93b2242ebecf6fd3
ifs19017 <ifs19017@students.del.ac.id>
2022-05-05 13:53:05 +0700

    add all

---
+++ resources/views/clusteringpm.blade.php
@@ -0,0 +1,441 @@
+!<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Clustering APP">
+  <meta name="author" content="Christoph Mittendorf">
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="theme-color" content="#ffffff">
+
+  <title>Clustering App</title>
+
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <!-- Font Style-->
+  <style media="screen">body {padding-top: 56px; font-family: 'Muli', sans-serif}</style>
+
+  <link rel="apple-touch-icon" sizes="180x180" href="../static/fav/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="../static/fav/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="../static/fav/favicon-16x16.png">
+  <link rel="manifest" href="../static/fav/site.webmanifest">
+
+  <!-- Custom font -->
+  <link href="https://fonts.googleapis.com/css?family=Assistant|Cabin|Indie+Flower|Muli|Nunito&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="../static/style.css">
+
+  <!-- JQuery -->
+  <script src="https://code.jquery.com/jquery-latest.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+
+      <a  class="navbar-brand" href="https://ml-clustering.ew.r.appspot.com/">
+        <img id="logo" style="width:30px !important; height:30px !important" src="../static/images/logo.png" alt="">
+      </a>
+
+      <a style="padding-left:7px; text-align:left" class="navbar-brand" href="">Clustering APP</a></br>
+      <a style="padding-left:7px" class="navbar-brand mobile" href="">(deployed on Google App Engine)</a></br></br>
+
+          <img id="logo mobile" style="width:40px !important; height:40px !important" src="https://fonts.gstatic.com/s/i/productlogos/google_cloud/v7/192px.svg" alt="Google Cloud">
+          <img id="logo mobile" style="width:40px !important; height:40px !important" src="../static/images/app_engine.png" alt="Google App Engine">
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"></button>
+
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="/index.html">Home<span class="sr-only">(current)</span> </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="about" href="#" onclick="aboutFunction()">About</a>
+                <script> function aboutFunction()
+                          {alert("Developer:  Dr. Christoph Mittendorf \nLocation:  Munich, Germany \nInternet:  http://www.ml-models.de \nE-Mail:  hello@epicml.net");}
+                </script>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="bd-pageheader text-center text-sm-left" style="background-color:#007eff; height:auto; color:white">
+    <div class="container">
+      <h1 id="heading_big" style="font-weight: bold; padding-top:22px; padding-bottom:4px">Clustering App
+        <img class="mobile" style="width:80px; margin-bottom:20px" src="../static/images/logo.png" alt="">
+        <small class="mobile"> (CSV Explorer)</small>
+      </h1>
+    </div>
+  </div>
+
+  <!-- Page Content -->
+  <div class="container" style="padding-top:30px">
+    <div class="row">
+      <div class="col-md-8">
+
+        <!-- ML Model Parameters ---------------------------------------------------------------------------------------------------------->
+        <div class="card mb-4">
+          <div class="card-body">
+            <h2 class="card-title">ML Model Parameters</h2>
+
+            <div class="ml_parameters menu">
+              <div id="first_ml_parameter" style="float:left; margin-right:25px; margin-bottom:10px">
+                <p style="text-align: center" title="The number of clusters that will be extracted by the ML model.">Number of Clusters*</p>
+                <select style="width:200px" id="first_ml_parameter_input" class="form-select" aria-label="Default select example"> </select>
+              </div>
+            </div>
+
+            <div id="second_ml_parameter" style="float:left; margin-right:25px; margin-bottom:10px">
+              <p style="text-align: center" title="The number of words that will describe the cluster.">Number of Words**</p>
+              <select style="width:200px" id="second_ml_parameter_input" class="form-select" aria-label="Default select example"></select>
+            </div>
+
+            <div id="third_qml_parameter" style="float:left; margin-left:0; margin-bottom:10px">
+              <p style="text-align: center" title="Number of Items that will be extracted per identified cluster.">Items per Cluster***</p>
+              <select style="width:200px" id="third_ml_parameter_input" class="form-select" aria-label="Default select example"></select>
+            </div>
+          </div>
+
+          <script>
+            //################################
+            //Create 100 value option dropdown
+            //################################
+            var selectClusters = document.getElementById("first_ml_parameter_input");
+            var contents;
+            //################################ First Dropdown
+              for (let i = 1; i <= 100; i++) {
+                if (i==10){contents += "<option selected>" + i + "</option>";}
+                else{contents += "<option>" + i + "</option>";}
+              };
+            selectClusters.innerHTML = contents;
+            //################################ Second Dropdown
+            var selectClusters = document.getElementById("second_ml_parameter_input");
+            contents = "";
+              for (let i = 1; i <= 100; i++) {
+                if (i==3){contents += "<option selected>" + i + "</option>";}
+                else{contents += "<option>" + i + "</option>";}
+              };
+            selectClusters.innerHTML = contents;
+            //################################ Thrid DropDown
+            contents = "";
+            selectClusters = document.getElementById("third_ml_parameter_input");
+              for (let i = 1; i <= 100; i++) {
+            if (i==10){contents += "<option selected>" + i + "</option>";}
+            else{contents += "<option>" + i + "</option>";}
+           };
+            selectClusters.innerHTML = contents;
+          </script>
+
+
+          <div class="card-footer text-muted">
+            <span> Please input the ml model parameters via the dropdown menus.</span> </br>
+            <span style="font-size:12px">* The number of clusters that will be extracted by the ML model</span> </br>
+            <span style="font-size:12px">** The number of words that will describe the cluster</span> </br>
+            <span style="font-size:12px">*** Number of Items that will be extracted per cluster</span>
+          </div>
+        </div>
+
+        <div style="margin-bottom:10px"><a href="https://storage.googleapis.com/public-ml-data-bucket/dummy_healthcare_data.csv">Download Dummy CSV File</a> </div>
+
+        <div>
+         <script type="text/javascript" src="../static/csv_reader.js"></script>
+         <input class="custom-file-upload " type="file" id="csvFileInput" onchange="handleFiles(this.files)" accept=".csv">
+        </div>
+
+    <div id="dvCSV"></div>
+
+<button id="run_button" style="margin-bottom: 25px; display:none" type="button" class="btn btn-primary" onclick="run_model();">Run analysis</button>
+</div>
+
+      <!-- Sidebar Widgets Column -->
+      <div class="col-md-4" style="margin-top:0px; padding:0">
+        <div class="card my-4" style="margin-top:0px !important">
+          <h5 class="card-header">Resources</h5>
+          <div class="card-body">
+            <div class="row">
+              <div class="col-lg-6">
+                <ul class="list-unstyled mb-0">
+                  <li>
+                    <a href="https://github.com/Cassini-chris/Clustering-App" target="_blank">GitHub repo</a>
+                  </li>
+                  <li>
+                    <a href="https://epicml.net/" target="_blank">EpicML.net</a>
+                  </li>
+                  <li>
+                    <a href="http://ml-models.de/" target="_blank">Ml-Models.de</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <ul class="list-unstyled mb-0">
+                  <li>
+                    <a href="https://en.wikipedia.org/wiki/Non-negative_matrix_factorization" target="_blank">NMF (wiki)</a>
+                  </li>
+                  <li>
+                    <a href="https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.NMF.html" target="_blank">NMF (soure)</a>
+                  </li>
+                  <li>
+                    <a href="https://cloud.google.com/" target="_blank">>> Google Cloud</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="card my-4" id="algorithm_card">
+          <h5 class="card-header">ML Algorithm</h5>
+          <div class="card-body" style="padding:0">
+
+            <a href="https://en.wikipedia.org/wiki/Non-negative_matrix_factorization" target="_blank"> <img style="max-width:100%;max-height:100%" src="../static/images/topic_ml2.png" alt="Algorithm"></a>
+          </div>
+        </div>
+
+        <div class="card my-4" id="explanation_card">
+          <h5 class="card-header">How to use it:</h5>
+          <div class="card-body">
+
+            <p> The Clustering APP is an application that identifies clusters by running topic modelling over your CSV file.
+              The application runs on Google Cloud and is powered by GAE and uses Non-negative matrix factorization to perform clustering.</p>
+            <p>
+              You can set the following ML parameters:
+            <ul>
+              <li>Number of clusters (topics)</li>
+              <li>Number of words per cluster</li>
+              <li>Number of features per cluster</li>
+            </ul>
+            <p>This is an unsupervised ml approach built for educational purposes. Hence, we recommend to evaluate the prediction carefully.
+            <p><i>#Developer: Christoph Mittendorf</i></p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+
+    <!-- PROGRESS BAR -->
+    <div class="box" id="loader_box" style="display:none; text-align:center;height:70px; margin:100px 0 100px 0">
+      <div class="col text-center">
+        <div class="loader" id="loader" style=" text-align:center"></div>
+      </div>
+    </div>
+
+
+    <!-- Result container -->
+    <div id="result_header" style="display:none">
+
+      <div id="topics" style="padding:8px 0 2px 20px; background-Color:#00264d; color:white; border-radius: 25px;height:60px">
+        <h2 class="card-title" style=" float:left">Results</h2>
+
+        <!-- Download Button for CSV -->
+        <button id="Download" onclick="run_download()" class="btn btn-primary" style="float:right; margin-right:30px">
+          <i class="fa fa-download"></i> Download
+        </button>
+      </div>
+    </div>
+
+    <div id="result_container"></div>
+  </div>
+
+  <script>
+    //##################################################################################################
+    //Function for :Downloading CSV Array
+    //##################################################################################################
+
+    function run_download() {
+
+      let csvContent = "data:text/csv;charset=utf-8,";
+
+      response_for_download.forEach(function(rowArray) {
+        let row = rowArray.join(",");
+        csvContent += row + "\r\n";
+
+      });
+      var encodedUri = encodeURI(csvContent);
+      var link = document.createElement("a");
+      link.setAttribute("href", encodedUri);
+      link.setAttribute("download", "clustering_data.csv");
+      document.body.appendChild(link); // Required for FF
+
+      link.click();
+    };
+  </script>
+
+
+  <script>
+    // Introduce message:
+    let message = "Message introduced - setting 10 as standard";
+    //console.log(message);
+    message = 10;
+    var new_array_ms = [10, 10, 10];
+    var response_for_download = [];
+
+    //##################################################################################################
+    // Run Model Function
+    //##################################################################################################
+
+    function run_model() {
+
+      var l = document.getElementById("loader_box");
+      if (l.style.display === "none") {
+        l.style.display = "block";
+        document.getElementById('loader_box').scrollIntoView();
+      };
+
+      // Hide explanation card after button click
+      var explanation_card = document.getElementById("explanation_card");
+      explanation_card.style.display = "none";
+
+      // Hide explanation card after button click
+      var algorithm_card = document.getElementById("algorithm_card");
+      algorithm_card.style.display = "none";
+
+
+      // Unhide results heading
+      var topics_header = document.getElementById("topics");
+      topics_header.style.display = "block";
+
+      // Number of ML Topics to identify
+      var first_ml_parameter_input = document.getElementById("first_ml_parameter_input").value;
+      if (first_ml_parameter_input == "Number of Clusters") {
+        first_ml_parameter_input = 10;
+      };
+
+      // Number of ML Topics to identify
+      var second_ml_parameter_input = document.getElementById("second_ml_parameter_input").value;
+      if (second_ml_parameter_input == "Number of Words") {
+        second_ml_parameter_input = 3;
+      };
+
+      // Number of ML Topics to identify
+      var third_ml_parameter_input = document.getElementById("third_ml_parameter_input").value;
+      if (third_ml_parameter_input == "Items per Cluster") {
+        third_ml_parameter_input = 10;
+      };
+
+      first_ml_parameter_input = parseInt(first_ml_parameter_input);
+      second_ml_parameter_input = parseInt(second_ml_parameter_input);
+      third_ml_parameter_input = parseInt(third_ml_parameter_input);
+
+      new_array_ms = [first_ml_parameter_input, second_ml_parameter_input, third_ml_parameter_input, full_text]
+
+      //##################################################################################################
+      // Transfer Function
+      //##################################################################################################
+
+      $.post("https://ml-clustering.ew.r.appspot.com/transfer", JSON.stringify(new_array_ms), function(response) {
+
+        //console.log("Response" + response);
+
+        // length_topics = Number of Clusters OVERALL
+        var length_topics = response[0].length;
+        // length_documents = Number of Items OVERALL
+        var length_documents = response[1].length;
+        // no_documents = Number of Items per Cluster
+        var no_documents = third_ml_parameter_input;
+
+        var current_topic = 0;
+        var current_topic_array = [];
+        var current_document = 0;
+        var current_document_array = [];
+
+        // For Downloading CSV
+        var combined_array = [];
+
+        const myNode = document.getElementById("result_container");
+        myNode.innerHTML = '';
+
+        console.log("length_topics: ", length_topics);
+        console.log("length_documents: ", length_documents);
+
+        // Hide Loader Box
+        document.getElementById('loader_box').style.display = "none";
+        document.getElementById('result_header').style.display = "block";
+
+        for (l = current_topic; l < length_topics; l++) {
+          //Dream Div - Cluster Name / Heading
+          var dream = document.createElement("div");
+          dream.style.backgroundColor = "#007bff";
+          dream.style.marginTop = "20px";
+          dream.style.borderRadius = "10px";
+          dream.style.color = "white";
+
+          var para = document.createElement("p");
+          para.style.paddingLeft = "20px";
+          para.style.fontWeight = "bold";
+
+          // Individual Documents per Trends
+          var node = document.createTextNode("CLUSTER " + (l + 1)+": " + response[0][l].replaceAll(",", ', ').toUpperCase());
+
+          // For Downloading CSV
+          let str_a = "CLUSTER: ";
+          let str_b = l + 1;
+          let str_c = ": ";
+          let str_d = (response[0][l]).toString();
+          let cluster_head = str_a + str_b + str_c + str_d;
+          cluster_head = cluster_head.toUpperCase();
+
+          combined_array.push([cluster_head]);
+
+          para.appendChild(node);
+          dream.appendChild(para);
+          //dream.appendChild("CLUSTER: " +para);
+
+          var element = document.getElementById("result_container");
+          element.appendChild(dream);
+
+          current_topic = current_topic + 1;
+          end_document = current_document + no_documents;
+
+          for (i = current_document; i < end_document; i++) {
+            var li = document.createElement("li");
+            li.style.padding = "0 0 0 20px";
+
+            var node = document.createTextNode(response[1][i].replaceAll(",", ', '));
+
+            // For Downloading CSV
+            //Escape Response
+            item_string = response[1][i].toString();
+            item_string = item_string.replaceAll("#", '-');
+            item_string = item_string.replaceAll(",", ', ');
+            combined_array.push([
+              [item_string]
+            ]);
+
+            li.appendChild(node);
+            var element = document.getElementById("result_container");
+            element.appendChild(li);
+            current_document = current_document + 1;
+
+          }
+
+          // Adding - Empty Row Between Clusters
+          combined_array.push([
+            []
+          ]);
+        }
+
+        console.log("Combined_array_final:", combined_array);
+        response_for_download = combined_array;
+      });
+    };
+  </script>
+
+
+  <!-- Footer -->
+  <footer class="py-5 bg-light" style="margin-top:20px; bottom: 0; width: 100%; padding-bottom:20px !important " >
+      <p class="m-0 text-center text-black">Mittendorf - Clustering-APP - 2021</p>
+      <div style="text-align:center; margin-top:10px ">
+        <span style="text-align:center; font-size:10px"> Disclaimer: This is not an officially supported Google product</span>
+      </div>
+  </footer>
+
+</body>
+</html>
